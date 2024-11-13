2fba278a5ee29a45a2ad9191e9b63dd634ae65c9
MyComputer <ibodov.aziz@yandex.ru>
2024-11-13 14:00:38 +0300

    Done

--- module2/Операторы if, elif, else.py
+++ module2/Операторы if, elif, else.py
@@ -1,5 +1,18 @@
-first = int(input('Введите первое число: '))
-second = int(input('Введите второе число: '))
-third = int(input('Введите третье число: '))
+def check_type():
+    numb = input('Введите число: ')
+    if numb.isdigit():
+        return int(numb)
+    print('Ошибка типа данных! Введите целое число. Попробуй еще раз!')
+    return check_type()
 
 
+first = check_type()
+second = check_type()
+third = check_type()
+
+if first == second and second == third:  # проверяем что все числа равны
+    print(3)
+elif first == second or first == third or second == third:  # ищем 2 одинаковых числа
+    print(2)
+else:
+    print(0)
