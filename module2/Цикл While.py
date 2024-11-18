fef1ce56923f922838dfa24d6d9dc9e9133b441f
MyComputer <ibodov.aziz@yandex.ru>
2024-11-18 17:59:58 +0300

    Done

--- module2/Цикл While.py
+++ module2/Цикл While.py
@@ -1,11 +1,13 @@
 list_numb = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 index = 0
+count = len(list_numb)
 
 while True:
-    if list_numb[index] < 0:
+    if count == 0 or list_numb[index] < 0:
         break
     print(list_numb[index])
     index += 1
+    count -= 1
 
 
