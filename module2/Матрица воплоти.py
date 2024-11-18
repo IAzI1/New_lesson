3443a2e0a88707a93907a8e49ca17aed0149cc4d
MyComputer <ibodov.aziz@yandex.ru>
2024-11-18 13:57:16 +0300

    Done

--- module2/Матрица воплоти.py
+++ module2/Матрица воплоти.py
@@ -1,6 +1,11 @@
 def get_matrix(n=2, m=2, value=10):
     matrix = []
 
+    # В случае передачи аргумента со значением 0 или меньше,
+    # будет возвращаться пустой список.
+    if n <= 0 or m <= 0 or value <= 0:
+        return matrix
+
     for x in range(n):
         matrix.append([]) # добавляем пустые списки
         for j in range(m):
@@ -15,7 +20,9 @@ result2 = get_matrix(3, 5, 42)
 
 result3 = get_matrix(4, 2, 13)
 
+result4 = get_matrix(2, 0, 1)
+
 print(result1)
 print(result2)
 print(result3)
-
+print(result4)
