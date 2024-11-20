d1eae55499ab07a3c7c9fe246696555b495fca73
MyComputer <ibodov.aziz@yandex.ru>
2024-11-20 16:01:28 +0300

    Done

--- module3/Способы вызова функции.py
+++ module3/Способы вызова функции.py
@@ -1,46 +1,42 @@
-def check_char(wrds, char_list):
-    """
-    :param wrds: str
-    :param char_list: list str которые нужно проверить
-    :return: False если хотяб в одном слове отсутствует строка из списка char_list
-    """
-    for wrd in wrds:
-        for char in char_list:
-            if not wrd.endswith(char):
-                return False
-
-    return True
-
-
-
-def send_email(message, addressee, sender='university.help@gmail.com'):
-    char_list = [".com", ".ru", ".net"]
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    char_list = ".com", ".ru", ".net"
 
     # если отправитель и получатель один и тот же адрес
-    if addressee == sender:
+    if recipient == sender:
         print('Нельзя отправить письмо самому себе!')
         return
 
     # Если нет знака @ в почте
-    if '@' not in addressee or '@' not in sender:
-        print('Отсутствует знак @ в почтах')
+    elif '@' not in recipient or '@' not in sender:
+        print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>')
         return
 
     # Если же addressee и sender совпадают, то вывести невозможно отправить письмо
-    if check_char([addressee, sender], char_list):
-        print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{addressee}>')
+    elif not recipient.endswith(char_list) or not sender.endswith(char_list):
+        print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>')
         return
 
-    # Если отправитель по умолчанию
-    if sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса <{sender}> на адрес <{addressee}>')
     else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{addressee}>')
+        print(f'Ваше сообщение: {message}')
+        # Если отправитель по умолчанию
+        if sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса <{sender}> на адрес <{recipient}>')
+        else:
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>')
 
 
-
-send_email('university.help@gmail.com', "university.helpg@mail.com")
-send_email('university.help@gmail.com', "university.help@gmail.com")
-send_email('university.help@gmail.com', "university.helpgmail.com")
-send_email('university.help@gmail.com', "university.helpg@mail")
-send_email('university.help@gmail.com', "university.helpg@mail.com", sender='iin@mail.ru')
+# Тестовые данные
+send_email('Это сообщение для проверки связи',
+           'vasyok1337@gmail.com')
+print()
+send_email('Вы видите это сообщение как лучший студент курса!',
+           'urban.fan@mail.ru',
+           sender='urban.info@gmail.com')
+print()
+send_email('Пожалуйста, исправьте задание',
+           'urban.student@mail.ru',
+           sender='urban.teacher@mail.uk')
+print()
+send_email('Напоминаю самому себе о вебинаре',
+           'urban.teacher@mail.ru',
+           sender='urban.teacher@mail.ru')
