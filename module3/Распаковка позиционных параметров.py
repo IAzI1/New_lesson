0f4c09fe85a7c89d0b22140e626be8e6ba3aa367
MyComputer <ibodov.aziz@yandex.ru>
2024-11-23 13:46:29 +0300

    Done

--- module3/Распаковка позиционных параметров.py
+++ module3/Распаковка позиционных параметров.py
@@ -1,10 +1,11 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
-# print_params()
-# print_params(b=25)
-# print_params(c=[1, 2, 3])
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
 
+print()
 ### Распаковка
 
 values_list = [
@@ -17,14 +18,15 @@ values_list2 = [
     [1, 2, 3]
 ]
 values_dict = {
+    'a': True,
     'b': 2,
-    'c': None
+    'c': 'string'
 }
-values_list2 = [
-    'str',
-    [1, 2, 3]
-]
+values_dict2 = {
+    'b': 2,
+    'c': 'string'
+}
 print_params(*values_list)
 print_params(5, *values_list2)
 print_params(**values_dict)
-print_params(3, **values_dict)
+print_params(3, **values_dict2)
