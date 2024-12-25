a07271e2f08e7a9149689a2ee5c191d1e31df0ce
MyComputer <ibodov.aziz@yandex.ru>
2024-12-25 22:46:24 +0300

    done

---
+++ module 6/6.3.py
@@ -0,0 +1,69 @@
+import random
+
+def multiply(n, multiplier):
+    return n * multiplier
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        if dz < 0:
+            print("It's too deep, i can't dive :(")
+            return
+        result = [x * self.speed for x in (dx, dy, dz)]
+        self._cords = result
+
+    def get_cords(self):
+        x, y, z = self._cords[0], self._cords[1], self._cords[2]
+        print(f'X: {x}, Y: {y}, Z: {z}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        elif self._DEGREE_OF_DANGER >= 5:
+            print("Be careful, i'm attacking you 0_0")
+
+    def speak(self):
+        print(f'{self.sound}')
+
+
+class Bird(Animal):
+    beak = True
+
+    @staticmethod
+    def lay_eggs():
+        print(f'Here are(is) {random.randint(1, 4)} eggs for you')
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._cords[2] -= abs(dz) * self.speed / 2
+
+
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
+    sound = 'Click-click-click'
+
+
+db = Duckbill(10)
+print(db.live)
+print(db.beak)
+db.speak()
+db.attack()
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
