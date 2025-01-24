1fba8dbf176b9d7438839a2d1590493806e0e028
MyComputer <ibodov.aziz@yandex.ru>
2025-01-24 13:48:03 +0300

    fix first_result

--- module9/2.py
+++ module9/2.py
@@ -4,7 +4,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 fird_string = first_strings + second_strings
 
-first_result = [string for string in first_strings if len(string) > 5]
+first_result = [len(string) for string in first_strings if len(string) > 5]
 second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 third_result = {x:len(x) for x in fird_string if len(x) % 2 == 0}
 
