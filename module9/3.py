73a46755d1c19585ef72f0189f3198f90309521a
MyComputer <ibodov.aziz@yandex.ru>
2025-01-24 16:06:52 +0300

    fix

--- module9/3.py
+++ module9/3.py
@@ -2,11 +2,8 @@ first = ['Strings', 'Student', 'Computers']
 
 second = ['Строка', 'Урбан', 'Компьютер']
 
-def is_len(a, b):
-    return len(a) == len(b)
-
 first_result = (abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s))
-second_result = (map(is_len, first, second))
+second_result =  (len(first[x]) == len(second[x]) for x in range(len(first)))
 
 print(type(first_result))
 print(list(first_result))
